0aecf6c7831b2bfa4e6fc69f02fb850a8d371957
HuguesRomain <huguesromainpro@gmail.com>
2019-02-04 23:49:07 +0100

    handleChange and handleSubmit function

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'; 
 
 class Formulaire extends Component {
+  state = {
+    message: ''
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
+
+  handleChange = event => {
+    const message = event.target.value
+  this.setState({ message })
+  }
+
   render(){
     return(
      <form
-     className='form'>
+     className='form'
+     onSubmit={this.handleSubmit}>
       <textarea 
+      value={this.state.message}
+      onChange={this.handleChange}
       maxLength='140'
       required/>
       <div className='info'>
